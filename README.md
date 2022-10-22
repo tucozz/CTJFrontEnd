f67ed2e8dc123da76fdfad83ff9ef84db7ae549f
Tucozz <114347055+tucozz@users.noreply.github.com>
2022-10-22 13:55:48 -0300

    Local Storage funcionando
    Local Storage ativo, porém a função DisplayTodos não funciona em nenhuma página, revizar isso. No mais, separei também os .js em dois para melhor funcionamento.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CTJFrontEnd
 Trabalho em conjunto para processo seletivo da CT Junior
+
+disponível para visualização em: https://tucozz.github.io/CTJFrontEnd/
---
+++ todolist.js
@@ -0,0 +1,115 @@
+window.addEventListener('load', () => {
+	todos = JSON.parse(localStorage.getItem('todos')) || [];
+	const newTodoForm = document.querySelector('#new-todo-form');
+
+	newTodoForm.addEventListener('submit', e => {
+		e.preventDefault();
+
+		const todo = {
+			content: e.target.elements.content.value,
+			category: e.target.elements.category.value,
+			description: e.target.elements.description.value,
+			done: false,
+			createdAt: new Date().getTime()
+		}
+
+		todos.push(todo);
+
+		localStorage.setItem('todos', JSON.stringify(todos));
+
+		// Reset the form
+		e.target.reset();
+
+		DisplayTodos();
+	})
+
+	DisplayTodos();  
+
+})
+
+function DisplayTodos () {
+	const todoList = document.querySelector('#todo-list');
+	todoList.innerHTML = '';
+
+	todos.forEach(todo => {
+		const todoItem = document.createElement('div');
+		todoItem.classList.add('todo-item');
+
+		const label = document.createElement('label');
+		const input = document.createElement('input');
+		const span = document.createElement('span');
+		const content = document.createElement('div');
+		const actions = document.createElement('div');
+		const edit = document.createElement('button');
+		const deleteButton = document.createElement('button');
+
+		input.type = 'checkbox';
+		input.checked = todo.done;
+		span.classList.add('bubble');
+
+		if (todo.category == 'pessoal') {
+			span.classList.add('pessoal');
+		} else if (todo.category == 'escola'){
+			span.classList.add('escola');
+		} else {
+			span.classList.add('trabalho');
+		}
+
+		content.classList.add('conteudo-todo');
+		actions.classList.add('actions');
+		edit.classList.add('edit');
+		deleteButton.classList.add('delete');
+
+		content.innerHTML = `<input type="text" value="${conteudo.todo}" readonly>`;
+		edit.innerHTML = 'Edit';
+		deleteButton.innerHTML = 'Delete';
+
+		label.appendChild(input);
+		label.appendChild(span);
+		actions.appendChild(edit);
+		actions.appendChild(deleteButton);
+		todoItem.appendChild(label);
+		todoItem.appendChild(content);
+		todoItem.appendChild(actions);
+
+		todoList.appendChild(todoItem);
+
+		if (todo.done) {
+			todoItem.classList.add('done');
+		}
+		
+		input.addEventListener('change', (e) => {
+			todo.done = e.target.checked;
+			localStorage.setItem('todos', JSON.stringify(todos));
+
+			if (todo.done) {
+				todoItem.classList.add('done');
+			} else {
+				todoItem.classList.remove('done');
+			}
+
+			DisplayTodos()
+
+		})
+
+		edit.addEventListener('click', (e) => {
+			const input = content.querySelector('input');
+			input.removeAttribute('readonly');
+			input.focus();
+			input.addEventListener('blur', (e) => {
+				input.setAttribute('readonly', true);
+				conteudo.todo = e.target.value;
+				localStorage.setItem('todos', JSON.stringify(todos));
+				DisplayTodos()
+
+			})
+		})
+
+		deleteButton.addEventListener('click', (e) => {
+			todos = todos.filter(t => t != todo);
+			localStorage.setItem('todos', JSON.stringify(todos));
+			DisplayTodos()
+		})
+
+	})
+}
